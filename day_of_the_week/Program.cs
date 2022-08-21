339bb05bbc4189383a7c89b97fb79cb99fb5f852
Sergey Oreshkin <oreshkin1111@gmail.com>
2022-08-21 23:23:03 +0300

    changed if to otherwise if

--- day_of_the_week/Program.cs
+++ day_of_the_week/Program.cs
@@ -2,10 +2,10 @@
 int day_of_the_week = int.Parse(Console.ReadLine());
 
 if(day_of_the_week == 1) Console.WriteLine("понедельник");
-if(day_of_the_week == 2) Console.WriteLine("вторник");
-if(day_of_the_week == 3) Console.WriteLine("среда");
-if(day_of_the_week == 4) Console.WriteLine("четверг");
-if(day_of_the_week == 5) Console.WriteLine("пятница");
-if(day_of_the_week == 6) Console.WriteLine("суббота");
-if(day_of_the_week == 7) Console.WriteLine("воскресенье");
-if(day_of_the_week > 7) Console.WriteLine("Введите число от 1 до 7");
+else if(day_of_the_week == 2) Console.WriteLine("вторник");
+else if(day_of_the_week == 3) Console.WriteLine("среда");
+else if(day_of_the_week == 4) Console.WriteLine("четверг");
+else if(day_of_the_week == 5) Console.WriteLine("пятница");
+else if(day_of_the_week == 6) Console.WriteLine("суббота");
+else if(day_of_the_week == 7) Console.WriteLine("воскресенье");
+else if(day_of_the_week > 7) Console.WriteLine("Введите число от 1 до 7");
