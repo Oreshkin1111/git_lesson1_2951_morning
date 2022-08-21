d27038ddc2ed40ee626ec24847a402d64ea94a71
Sergey Oreshkin <oreshkin1111@gmail.com>
2022-08-21 22:38:18 +0300

    add square

---
+++ square/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число A: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int b = int.Parse(Console.ReadLine());
+
+if(a * a == b)
+{
+    Console.WriteLine("число B является квадратом числа A");
+}
+else if(a == 0)
+{
+    Console.WriteLine("0 является квадратом 0");
+}
+else
+{
+    Console.WriteLine("число B не является квадратом числа A");
+}
