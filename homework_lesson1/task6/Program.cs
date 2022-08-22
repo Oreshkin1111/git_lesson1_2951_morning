15a77f8f10ea466c56320f481ee1f3ee967fba32
Sergey Oreshkin <oreshkin1111@gmail.com>
2022-08-22 15:50:04 +0300

    add task 6

---
+++ homework_lesson1/task6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+if(a %2 == 0)
+{
+    Console.Write("Введенное число является четным");
+}
+else
+{
+    Console.Write("Введенное число является нечетным");
+}
