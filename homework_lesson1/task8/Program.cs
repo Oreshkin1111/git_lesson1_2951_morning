46790157d7d28a68a4fc61de9c4bf7de3ea8ea72
Sergey Oreshkin <oreshkin1111@gmail.com>
2022-08-22 16:53:30 +0300

    add Program.cs

---
+++ homework_lesson1/task8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите целое число: ");
+int m = int.Parse(Console.ReadLine());
+int n = 1;
+for (int i = n; i <= m; i++)
+{
+    if (i % 2 == 0)
+    Console.Write(i + " ");
+}
+
+
+
