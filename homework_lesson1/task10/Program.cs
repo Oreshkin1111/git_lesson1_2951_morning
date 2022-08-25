38d044f357abd4ca152af6709e7fbff3ca44d1f7
Sergey Oreshkin <oreshkin1111@gmail.com>
2022-08-25 18:15:25 +0300

    added a function to the task10

--- homework_lesson1/task10/Program.cs
+++ homework_lesson1/task10/Program.cs
@@ -3,15 +3,16 @@
 //645 -> 5
 //78 -> третьей цифры нет
 //32679 -> 6
-
-Console.WriteLine("Введите число больше 99: ");
-string s = Console.ReadLine();
-char[] dig_mas = new char[s.Length];
-for (int i = 0; i < s.Length; i++)
+void ShowThirdDigit()
 {
-    dig_mas[i] = s[i];
-    Console.Write(dig_mas[i] + ", ");
-}
+    Console.WriteLine("Введите число больше 99: ");
+    string s = Console.ReadLine();
+    char[] dig_mas = new char[s.Length];
+    for (int i = 0; i < s.Length; i++)
+    {
+        dig_mas[i] = s[i];
+        Console.Write(dig_mas[i] + ", ");
+    }
     if (s.Length > 2)
     {
         Console.WriteLine("Третья цифра числа: " + dig_mas[2]);
@@ -20,6 +21,9 @@ for (int i = 0; i < s.Length; i++)
     {
         Console.WriteLine("Вы ввели число меньше 100");
     }
+}
+
+ShowThirdDigit();
 
 
 
