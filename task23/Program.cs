5d74c91d98db9f94762b30645aaaee5ebfca0a64
Sergey Oreshkin <oreshkin1111@gmail.com>
2022-08-26 22:56:39 +0300

    added task 23

---
+++ task23/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125 */
+
+ void TableOfSquares()
+{
+    Console.WriteLine("Введите число N ");
+    double N = Convert.ToInt32(Console.ReadLine());
+    if (N > 1)
+    {
+        for (double i = 1; i <= N; i++)
+        {
+            double index = Math.Pow(i, 3);
+            Console.Write(index + ", ");
+        }
+    }
+    else Console.WriteLine("Введите положительное число");
+}
+try
+{
+    TableOfSquares();
+}
+catch
+{
+    Console.WriteLine("Введите положительное число");
+} 
