89346e5bdade906b05c78c440dd54826fbdf7a2a
Sergey Oreshkin <oreshkin1111@gmail.com>
2022-08-27 23:19:39 +0300

    added task 19

---
+++ task19/Program.cs
@@ -0,0 +1,29 @@
+﻿void Palindrome()
+{
+    int number;
+    int result = 0;
+    Console.Write("Введите пятизначное число: ");
+    number = Convert.ToInt32(Console.ReadLine());
+    int res = number;
+    if (number > 9999 && number < 100000)
+    {
+        while (number > 0)
+        {   
+            result *= 10;
+            result += number % 10;
+            number /= 10;
+        }
+        Console.WriteLine(result);
+        if (result == res) Console.WriteLine("Это палиндром ");
+        else Console.WriteLine("Введите другое пятизначное число ");
+    }
+    else Console.WriteLine("Введите пятизначное число ");
+}
+try
+{
+   Palindrome();
+}
+catch
+{
+   Console.WriteLine("Нужно вводить цифры ");
+} 
