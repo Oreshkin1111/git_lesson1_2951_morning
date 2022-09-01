14974dd8ae8cbdc8e90ed62f7a47d1bc1befa010
Sergey Oreshkin <oreshkin1111@gmail.com>
2022-09-01 14:47:31 +0300

    added task 29

---
+++ task29/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Задача 29: Напишите программу, которая задаёт массив из 8 элементов 
+и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33] */
+
+int N = 8;
+int [] numbers = new int[N];
+ for(int i=0; i<N; i++)
+ {
+     numbers[i] = new Random().Next(1,100);
+     if (i!=N-1) Console.Write($"{numbers[i]}, ");
+     else Console.WriteLine($"{numbers[i]}");
+ }
